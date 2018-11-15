e1b486bb0c7b468f5a663a7186f7e4469a34bf8f
Pradyumn Singh <22pradyumns@gmail.com>
2018-11-15 15:38:51 +0000

    adding user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,9 @@ class User < ActiveRecord::Base
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if first_name || last_name
+    "Anonymous"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113070034) do
+ActiveRecord::Schema.define(version: 20181115045458) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20181113070034) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
